55de5feaa7f4adef6bffcadc50e331c1f311bb08
T1Erick <guevaraerick49@gmail.com>
2023-08-25 19:17:14 -0500

    Cambios

--- src/app/pages/list-patient/list-patient.component.ts
+++ src/app/pages/list-patient/list-patient.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-patient',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-patient.component.css']
 })
 export class ListPatientComponent {
+  constructor(private router: Router){}
+  navigateToCita() {
+    this.router.navigateByUrl("/dashboard/medical-appointment");
+  }
+
 
 }
--- src/app/pages/register-patient/register-patient.component.ts
+++ src/app/pages/register-patient/register-patient.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-patient',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-patient.component.css']
 })
 export class RegisterPatientComponent {
-
+  
 }
